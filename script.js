fd34e291a6560c5d2d892946c4b47dc5b2399cd0
Anaid-Ariwany <anaidariwany@gmail.com>
2026-04-07 14:54:17 +0300

    Setup the landing page for the repo.

---
+++ script.js
@@ -0,0 +1,40 @@
+const projects = [
+    {
+        name: "",
+        description: "",
+        image: "",
+        live: "https://anaid-ariwany.github.io/The-Odin-Project-Learning/HTML/Projects/Recipes/",
+        github: "https://github.com/Anaid-Ariwany/The-Odin-Project-Learning/tree/main/HTML/Projects/Recipes",
+        source: ""
+    }
+];
+
+
+const projectContainer = document.querySelector(".projectContainer");
+
+projects.forEach(project => {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("card");
+    projectCard.innerHTML = `
+        <div class="row g-1">
+            <div class="col-xxl-12 imagediv">
+                <img src="${project.image}" class="card-img-top" alt="${project.name}">
+            </div>
+            <div class="col-xxl-12">
+                <div class="card-body">
+                    <h4 class="card-title poppins-semibold">${project.name}</h4>
+                    <p class="card-text">${project.description}</p>
+                    <div class="projectButtons">
+                        <button onclick="window.open('${project.live}', '_blank', 'noopener,noreferrer')"
+                            class="lightButton">Live Demo</button>
+                        <button onclick="window.open('${project.github}', '_blank', 'noopener,noreferrer')"
+                            class="darkButton">GitHub Repo</button>
+                        <button onclick="window.open('${project.source}', '_blank', 'noopener,noreferrer')"
+                            class="lightButton">Source</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    projectContainer.appendChild(projectCard);
+});
