2bad50ec8876700c31aaed83e07648abb68cf15f
Anaid-Ariwany <anaidariwany@gmail.com>
2026-06-13 18:30:56 +0300

    Notes: Completed notes on Setting Up React Environment,
    - Created my first app using React.

---
+++ React/Projects/my-first-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="welcome-screen">
+      <p className="eyebrow">React practice</p>
+      <h1>My first time learning React</h1>
+      <p className="message">
+        This page is a replacement for the default Vite starter template. This is my message.
+      </p>
+    </main>
+  )
+}
+
+export default App
